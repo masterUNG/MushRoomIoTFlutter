39e64dca76bca9b2af9c642b778c3dded367fae2
wanpracha <wanpracha.n@rmutsv.ac.th>
2019-06-11 16:00:50 +0700

    finish 11June2019

--- lib/screens/authen.dart
+++ lib/screens/authen.dart
@@ -8,17 +8,48 @@ class Authen extends StatefulWidget {
 class _AuthenState extends State<Authen> {
   //Explcit
   double amount = 180.0;
+  double size = 250.0;
   //Method
+  Widget emailTextFormField() {
+    return Container(margin:EdgeInsets.only(top: 10.0),
+      alignment: Alignment.center,
+      child: Container(
+        width: size,
+        child: TextFormField(
+          decoration: InputDecoration(
+            labelText: 'Email: ',
+            hintText: 'you@email.com',
+          ),
+        ),
+      ),
+    );
+  }
+  
+  Widget passwordTextFormField() {
+    return Container(margin:EdgeInsets.only(top: 10.0),
+      alignment: Alignment.center,
+      child: Container(
+        width: size,
+        child: TextFormField(
+          decoration: InputDecoration(
+            labelText: 'Password: ',
+            hintText: 'More 6 Charactor',
+          ),
+        ),
+      ),
+    );
+  }
+
   Widget showName() {
     return Container(
-      margin: EdgeInsets.only(top: 15.0),
+      margin: EdgeInsets.only(top: 16.0),
       child: Text(
         'MushRoom Farm IoT',
         style: TextStyle(
-          fontSize: 30.0,
-          color: Colors.orange[500],
-          fontWeight: FontWeight.bold, fontFamily: 'GloriaHallelujah'
-        ),
+            fontSize: 30.0,
+            color: Colors.orange[500],
+            fontWeight: FontWeight.bold,
+            fontFamily: 'GloriaHallelujah'),
       ),
     );
   }
@@ -33,12 +64,12 @@ class _AuthenState extends State<Authen> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
+    return Scaffold(resizeToAvoidBottomPadding: false,
       body: Container(
-        padding: EdgeInsets.only(top: 70.0),
+        padding: EdgeInsets.only(top: 20.0),
         alignment: Alignment(0, -1),
         child: Column(
-          children: <Widget>[showLogo(), showName()],
+          children: <Widget>[showLogo(),showName(), emailTextFormField(),passwordTextFormField()],
         ),
       ),
     );
