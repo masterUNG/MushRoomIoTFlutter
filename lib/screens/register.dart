d3dab84e3d255bc934990c9fe82940aba27fcf7a
wanpracha <wanpracha.n@rmutsv.ac.th>
2019-06-12 15:22:00 +0700

    Authen Complete

--- lib/screens/register.dart
+++ lib/screens/register.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/rendering.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 
 class Register extends StatefulWidget {
   @override
@@ -7,75 +8,139 @@ class Register extends StatefulWidget {
 }
 
 class _RegisterState extends State<Register> {
+  // Explicit
+  final formKey = GlobalKey<FormState>(); //create sending variable
+  String nameString, emailString, passwordString, phoneString;
+
+  // Method
   Widget uploadButton() {
     return IconButton(
       icon: Icon(Icons.cloud_upload),
       iconSize: 36.0,
-      onPressed: () {},
+      onPressed: () {
+        print('You Click Upload');
+        if (formKey.currentState.validate()) {
+          formKey.currentState.save(); //save data from form to variable
+          uploadToFirebase();
+        }
+      },
     );
+  }
+
+  void uploadToFirebase() async {
+    //async is a thread
+    print(
+        'Name = $nameString,email = $emailString, pass = $passwordString, phone = $phoneString');
+    FirebaseAuth firebaseAuth = FirebaseAuth.instance;
+    await firebaseAuth
+        .createUserWithEmailAndPassword(
+            email: emailString, password: passwordString)
+        .then((objValue) {
+      String uidString = objValue.uid.toString();
+      print('uid ==> $uidString');
+    }).catchError((objValue) {
+      String error = objValue.message; //show error
+      print('error ==> $error');
+    });
   }
 
   Widget nameText() {
     return TextFormField(
       decoration: InputDecoration(
-          icon: Icon(
-            Icons.face,
-            color: Colors.blue[500],
-            size: 36.0,
-          ),
-          labelText: 'Name :',
-          labelStyle: TextStyle(color: Colors.orange[500]),
-          helperText: 'First Name and Last Name',
-          helperStyle: TextStyle(
-              color: Colors.orange[300], fontStyle: FontStyle.italic)),
+        icon: Icon(
+          Icons.face,
+          color: Colors.blue[500],
+          size: 36.0,
+        ),
+        labelText: 'Name :',
+        labelStyle: TextStyle(color: Colors.orange[500]),
+        helperText: 'First Name and Last Name',
+        helperStyle:
+            TextStyle(color: Colors.orange[300], fontStyle: FontStyle.italic),
+      ),
+      validator: (String value) {
+        if (value.length == 0) {
+          return 'Please Fill Name in Blank';
+        }
+      },
+      onSaved: (String value) {
+        nameString = value.trim();
+      },
     );
   }
 
   Widget nameEmail() {
     return TextFormField(
       decoration: InputDecoration(
-          icon: Icon(
-            Icons.email,
-            color: Colors.blue[500],
-            size: 36.0,
-          ),
-          labelText: 'Email :',
-          labelStyle: TextStyle(color: Colors.orange[500]),
-          helperText: 'abce@gmail.com',
-          helperStyle: TextStyle(
-              color: Colors.orange[300], fontStyle: FontStyle.italic)),
+        icon: Icon(
+          Icons.email,
+          color: Colors.blue[500],
+          size: 36.0,
+        ),
+        labelText: 'Email :',
+        labelStyle: TextStyle(color: Colors.orange[500]),
+        helperText: 'abce@gmail.com',
+        helperStyle:
+            TextStyle(color: Colors.orange[300], fontStyle: FontStyle.italic),
+      ),
+      validator: (String namevalue) {
+        if (!((namevalue.contains('@')) && (namevalue.contains('.')))) {
+          return 'Wrong Email Format you@email.com';
+        }
+      },
+      onSaved: (String value) {
+        emailString = value.trim();
+      },
     );
   }
 
   Widget namePassword() {
     return TextFormField(
       decoration: InputDecoration(
-          icon: Icon(
-            Icons.lock,
-            color: Colors.blue[500],
-            size: 36.0,
-          ),
-          labelText: 'Password :',
-          labelStyle: TextStyle(color: Colors.orange[500]),
-          helperText: '8 Charactor',
-          helperStyle: TextStyle(
-              color: Colors.orange[300], fontStyle: FontStyle.italic)),
+        icon: Icon(
+          Icons.lock,
+          color: Colors.blue[500],
+          size: 36.0,
+        ),
+        labelText: 'Password :',
+        labelStyle: TextStyle(color: Colors.orange[500]),
+        helperText: '8 Charactor',
+        helperStyle:
+            TextStyle(color: Colors.orange[300], fontStyle: FontStyle.italic),
+      ),
+      validator: (String passwordvalue) {
+        if (passwordvalue.length <= 7) {
+          return 'Minimum Password is 8 Charactor';
+        }
+      },
+      onSaved: (String value) {
+        passwordString = value.trim();
+      },
     );
   }
 
   Widget namePhone() {
     return TextFormField(
       decoration: InputDecoration(
-          icon: Icon(
-            Icons.phone,
-            color: Colors.blue[500],
-            size: 36.0,
-          ),
-          labelText: 'Phone :',
-          labelStyle: TextStyle(color: Colors.orange[500]),
-          helperText: '0891234567',
-          helperStyle: TextStyle(
-              color: Colors.orange[300], fontStyle: FontStyle.italic)),
+        icon: Icon(
+          Icons.phone,
+          color: Colors.blue[500],
+          size: 36.0,
+        ),
+        labelText: 'Phone :',
+        labelStyle: TextStyle(color: Colors.orange[500]),
+        helperText: '0891234567',
+        helperStyle:
+            TextStyle(color: Colors.orange[300], fontStyle: FontStyle.italic),
+      ),
+      validator: (String phonevalue) {
+        if (phonevalue.length == 0) {
+          return 'Please Fill Phone in Blank';
+        }
+      },
+      onSaved: (String value) {
+        phoneString = value.trim();
+      },
     );
   }
 
@@ -88,15 +153,18 @@ class _RegisterState extends State<Register> {
         title: Text('Register'),
         actions: <Widget>[uploadButton()],
       ),
-      body: Container(
-        padding: EdgeInsets.only(top: 80.0, left: 50.0, right: 50.0),
-        child: ListView(
-          children: <Widget>[
-            nameText(),
-            nameEmail(),
-            namePassword(),
-            namePhone()
-          ],
+      body: Form(
+        key: formKey,
+        child: Container(
+          padding: EdgeInsets.only(top: 80.0, left: 50.0, right: 50.0),
+          child: ListView(
+            children: <Widget>[
+              nameText(),
+              nameEmail(),
+              namePassword(),
+              namePhone()
+            ],
+          ),
         ),
       ),
     );
