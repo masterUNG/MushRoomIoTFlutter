8ab65d3de79e9481473179ca77fdfcd0ee55dbd7
wanpracha <wanpracha.n@rmutsv.ac.th>
2019-06-14 16:00:17 +0700

    Fog Fan Ligh Ok

--- lib/screens/show_service.dart
+++ lib/screens/show_service.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:webview_flutter/webview_flutter.dart';
+import 'package:firebase_database/firebase_database.dart';
 
 class ShowServic extends StatefulWidget {
   @override
@@ -10,6 +11,29 @@ class _ShowServicState extends State<ShowServic> {
   // Explicit
   String url1 =
       'https://thingspeak.com/channels/437885/charts/1?bgcolor=%23ffffff&color=%23d62020&dynamic=true&results=60&type=line&update=15';
+  int fogInt, fanInt, lightInt;
+  FirebaseDatabase firebaseDatabase = FirebaseDatabase.instance;
+  Map<dynamic, dynamic> map;
+
+  @override
+  void initState() {
+    super.initState();
+    getValueFromFirebase();
+  }
+
+  void getValueFromFirebase() async {
+    DatabaseReference databaseReference =
+        await firebaseDatabase.reference().child('IoT').once().then((objValue) {
+      map = objValue.value;
+      setState(() {
+        fogInt = map['Fog'];
+        fanInt = map['Fan'];
+        lightInt = map['Light'];
+        print('fog = $fogInt, fan = $fanInt,light=$lightInt');
+      });
+    });
+  }
+
   // Method
   Widget showGraph(String urlString) {
     return WebView(
@@ -36,18 +60,49 @@ class _ShowServicState extends State<ShowServic> {
     );
   }
 
-  Widget showPanal(String labelString, String valueString) {
+  Widget showPanal(String labelString, int textInt) {
     return Expanded(
       child: Column(
         children: <Widget>[
-          Text(valueString),
+          Text('$textInt'),
           RaisedButton(
             child: Text(labelString),
-            onPressed: () {},
+            onPressed: () {
+              int valueInt =0;
+              if (labelString=='Fog') {
+                valueInt = fogInt+1;
+                if (valueInt==2) {
+                  valueInt=0;
+                }
+              }else if(labelString=='Fan'){
+                  valueInt = fanInt+1;
+                if (valueInt==2) {
+                  valueInt=0;
+                }
+              }else{
+                valueInt = lightInt+1;
+                if (valueInt==2) {
+                  valueInt=0;
+              }
+              }
+              editFirebase(labelString,valueInt);
+            },
           )
         ],
       ),
     );
+  }
+
+  void editFirebase(String nodeString, int value) async {
+    print('node ==>$nodeString');
+    map['$nodeString'] = value;
+    await firebaseDatabase.reference().child('IoT').set(map).then((objValue) {
+      print('$nodeString Success');
+      getValueFromFirebase();
+    }).catchError((objValue) {
+      String error = objValue.message;
+      print('error ==>$error');
+    });
   }
 
   Widget showThinkSpeak(String urlString) {
@@ -76,9 +131,9 @@ class _ShowServicState extends State<ShowServic> {
               width: 240.0,
               child: Row(
                 children: <Widget>[
-                  showPanal('Fog', '0'),
-                  showPanal('Fan', '0'),
-                  showPanal('Light', '0')
+                  showPanal('Fog', fogInt),
+                  showPanal('Fan', fanInt),
+                  showPanal('Light', lightInt)
                 ],
               ),
             ),
